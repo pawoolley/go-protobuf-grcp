d8f7079ea7e24418b5d4179aa8bcb987cde2aefa
Paul Woolley <pwoolley@cloudbees.com>
2022-10-19 11:31:05 +0100

    Enable server reflection

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go-echo/generated"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 	"time"
@@ -17,8 +18,9 @@ func (s *helloWorldServer) SayHi(req *pb.Request, server pb.HelloWorld_SayHiServ
 	log.Printf("Received: %v\n", req.Name)
 
 	for i := 0; i < 5; i++ {
+		//goland:noinspection GoUnhandledErrorResult
 		server.Send(&pb.Response{
-			Message: fmt.Sprintf("Hi %v", req.Name),
+			Message: fmt.Sprintf("Hi, %v", req.Name),
 		})
 		time.Sleep(time.Second)
 	}
@@ -30,6 +32,10 @@ func main() {
 	lis, _ := net.Listen("tcp", "localhost:65535")
 	grpcServer := grpc.NewServer()
 	pb.RegisterHelloWorldServer(grpcServer, &helloWorldServer{})
+	// Register reflection service on gRPC server. Without doing this, IJ's built-in HTTP client
+	// won't do GRPC requests and fails with a "Reflective gRPC call timed out" error.
+	reflection.Register(grpcServer)
 	log.Println("Listening...")
+	//goland:noinspection GoUnhandledErrorResult
 	grpcServer.Serve(lis)
 }
